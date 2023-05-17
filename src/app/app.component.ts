36da086d9ccb85a18be9d1f33c9c125baaa6e633
Shashank Dhakate <shashankdkte@gmail.com>
2023-05-17 18:01:50 +0530

    Added Events and properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  lengthValue = 0;
+  password = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  onInputValue(event: any) {
+    const parsedValue = parseInt(event.target.value);
+    console.log(parsedValue);
+    this.lengthValue = parsedValue;
+  }
   onButtonClick() {
-    console.log('Button was clicked');
+    let validChars = '';
+    let letters = 'abcdefghijklmnopqrstuvwxyz';
+    let numbers = '1234567890';
+    let symbols = '!@#$%^&*()';
+    let generatedPassword = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    for (let i = 0; i < this.lengthValue; i++) {
+      let indexValue = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[indexValue];
+    }
+    this.password = generatedPassword;
+  }
+  onChangeUseLetter() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumber() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbol() {
+    this.includeSymbols = !this.includeSymbols;
   }
 }
